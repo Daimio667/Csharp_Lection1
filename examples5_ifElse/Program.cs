29e8761d7885036e47e2b221f9370da5dacfd715
Oleg <daimio667@gmail.com>
2022-08-28 18:20:41 +0300

    IfElse

---
+++ examples5_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter you name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "natasha")
+{
+    Console.WriteLine("Wow, that's NATASHA!!!");
+}
+else
+{
+    System.Console.Write("Hello, ");
+    System.Console.WriteLine(username);
+}
