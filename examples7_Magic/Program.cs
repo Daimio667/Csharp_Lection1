360b654f286f8ddb46750e9a8ead85d5a49ecbca
Oleg <daimio667@gmail.com>
2022-08-28 21:56:54 +0300

    Magic

---
+++ examples7_Magic/Program.cs
@@ -0,0 +1,49 @@
+﻿ // This is street magic))
+ //Console.Clear()
+ //Console.SetCursorPosition(10, 4);
+ //System.Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("-");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("=");
+
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 10)
+{
+    int who = new Random().Next(0, 3); //(0;3) 0 1 2 3
+    if(who == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+
+    if(who == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+
+    if(who == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+    
+}
